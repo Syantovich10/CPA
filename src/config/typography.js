62ba75a6a5fe865d3b03a836e4491af23704dabc
KENGAN <kengangenkay@gmail.com>
2026-07-16 17:13:00 +0300

    made it simple

--- src/config/typography.js
+++ src/config/typography.js
@@ -1,24 +1,27 @@
-export const TEXT_VARIANTS = {
-  accent: 'font-halvar text-[16px] leading-[20px]',
-  bodyLarge: 'font-stolzl text-[20px] leading-[24px]',
-  contentLarge: 'text-[20px] leading-[24px]',
-  actionLarge: 'font-halvar text-[20px] leading-[24px]',
-  form: 'font-stolzl text-[14px]',
-  modal: 'font-stolzl text-[16px]',
-  modalAction: 'font-halvar text-[14px]',
-  navigation: 'font-halvar text-[32px]',
-  marquee: 'font-halvar text-[35px] leading-normal',
-}
-
-export const TEXT_WEIGHTS = {
-  light: 'font-light',
-  regular: 'font-normal',
-  medium: 'font-medium',
-  bold: 'font-bold',
-}
-
-export const HEADING_VARIANTS = {
+export const HEADING_STYLES = {
   h1: 'font-halvar text-[38px] leading-[90%] font-bold',
   h2: 'font-halvar text-[20px] leading-[90%] font-bold',
   h3: 'font-halvar text-[28px] leading-[28px] font-bold',
+}
+
+export const TEXT_STYLES = {
+  halvar14Medium: 'font-halvar text-[14px] font-medium',
+
+  halvar16Bold: 'font-halvar text-[16px] leading-[20px] font-bold',
+
+  halvar20Bold: 'font-halvar text-[20px] leading-[24px] font-bold',
+
+  halvar32Bold: 'font-halvar text-[32px] font-bold',
+
+  halvar35Light: 'font-halvar text-[35px] leading-normal font-light',
+
+  stolzl14Light: 'font-stolzl text-[14px] font-light',
+
+  stolzl14Medium: 'font-stolzl text-[14px] font-medium',
+
+  stolzl16Light: 'font-stolzl text-[16px] font-light',
+
+  stolzl20Regular: 'font-stolzl text-[20px] leading-[24px] font-normal',
+
+  text20Regular: 'text-[20px] leading-[24px] font-normal',
 }
