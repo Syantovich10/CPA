814524785038556f33f8adea4d3a13435c8f9bc7
Gleb <privpok2007@gmail.com>
2026-07-17 23:53:47 +0300

    chore: clean up App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import ThreeDButton from './components/Button/ThreeDButton.jsx'
-
 function App() {
-  return (
-    <div>
-      <ThreeDButton className="m-9">Click ME</ThreeDButton>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
