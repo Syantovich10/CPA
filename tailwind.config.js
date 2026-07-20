e2795414b51e99aad84a4033271540fc7ace9e3a
KENGAN <kengangenkay@gmail.com>
2026-07-20 16:55:01 +0300

    initialize branch & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#0C090E',
+        white: '#FFFFFF',
+        purple: '#9E13E0',
+        yellow: '#FEE97D',
+        surface: '#14091A',
+      },
+    },
   },
   plugins: [],
 }
