0e5f73a5f91b2062b442c357feb503b5c9725290
Gleb <privpok2007@gmail.com>
2026-07-25 19:33:35 +0300

    add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,57 @@
+import arrowIcon from '@/assets/icons/arrow.svg'
+import { TEXT_STYLES } from '@/config/typography.js'
+
+function Footer() {
+  const linkClass = `
+    ${TEXT_STYLES.halvar20Bold}
+    text-yellow
+    uppercase
+    whitespace-nowrap
+  `
+
+  const underlinedLinkClass = `${linkClass} underline`
+
+  return (
+    <footer className="hidden bg-dark lg:block">
+      <div
+        className="
+          mx-auto
+          flex
+          w-full
+          max-w-[1440px]
+          items-center
+          justify-between
+          px-4
+          py-6
+          sm:px-8
+          lg:px-16
+        "
+      >
+        <div className="flex items-center gap-4 sm:gap-6 lg:gap-8">
+          <a href="#" className={underlinedLinkClass}>
+            instagram
+          </a>
+
+          <a href="#" className={underlinedLinkClass}>
+            telegram
+          </a>
+
+          <a href="#" className={underlinedLinkClass}>
+            linkedin
+          </a>
+        </div>
+
+        <a href="#top" className={linkClass}>
+          <span className="underline">scroll to top</span>
+          <img
+            src={arrowIcon}
+            alt="arrowTop"
+            className="ml-2 inline-block size-7.5 pb-1.5"
+          />
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
 import HeaderSection from '@/sections/HeaderSection/HeaderSection'
+import Footer from '@/components/Footer/Footer.jsx'
 
 function HomePage() {
-  return <HeaderSection />
+  return (
+    <div>
+      <HeaderSection />
+      <Footer></Footer>
+    </div>
+  )
 }
 
 export default HomePage
