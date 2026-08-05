7580a60021380728b407bc51f3eb3ce85d54351b
Abdullaev Abdulaziz <abdulazizfaactory@gmail.com>
2026-08-05 22:23:20 +0500

    chore: remove ApplicationForm from HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,9 @@
-import ApplicationForm from '@/components/Form/ApplicationForm'
 import Header from '@/components/Header/Header'
 
 function HomePage() {
   return (
     <>
       <Header />
-      <div className="flex min-h-screen items-center justify-center bg-surface p-6">
-        <ApplicationForm onClose={() => {}} />
-      </div>
     </>
   )
 }
